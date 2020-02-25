7c785ada2dfaf76b2666599d0097f3fea08e6925
Tyler <raymond.tylerr@gmail.com>
2020-02-24 21:42:07 -0500

    Cleaned up code created folder structure

--- Reddit/_Test.py
+++ Reddit/_Test.py
@@ -1,16 +1,3 @@
-from pprint import pprint
-import requests
-import Reddit_GetCommentData
-
-my_headers = {'User-agent' : 'MyUserAgent'}
-my_filtercriteria = {'limit':'1'}
-
-try:
-    r = requests.get('http://www.reddit.com/r/WallStreetBets/hot/.json',headers= my_headers,params=my_filtercriteria)
-    r.raise_for_status()
-        
-    pprint(r.json()) 
-except Exception as e:
-    print(str(e))
+import rcomments
 
 
